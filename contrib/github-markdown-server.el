b985b6a03bcf179f872df15db939595f12310c31
Alex Knowles <ark@looker.com>
2014-11-29 03:19:26 -0800

    emacs-lisp file no longer need my little p program.

--- contrib/github-markdown-server.el
+++ contrib/github-markdown-server.el
@@ -9,11 +9,14 @@
 
 (defvar github-markdown-servers '() "The servers we have running.")
 (defvar github-markdown-server-port 7494 "The next available server port.")
+(defvar github-markdown-server-browser "Google Chrome" "Application to use with osx's open -a to open files.")
 
 (defun git-base (&optional file-name)
   "Get the base of a git repository for FILE-NAME (or the current buffer)."
   (interactive (buffer-file-name))
-  (shell-command-to-string (concat "p -t -g " (shell-quote-argument buffer-file-name))))
+  (shell-command-to-string
+    (concat "git -C " (shell-quote-argument (directory-file-name file-name))
+      " rev-parse --show-toplevel 2>/dev/null")))
 
 (defun github-markdown-serve  (&optional file-name)
   "Get the base of a git repository for FILE-NAME (or the current buffer)."
@@ -44,18 +47,26 @@
                   (substring file-name (length (car server))))))))
         (progn
           (if server
-            (progn (message (concat "re-starting server on port " (number-to-string (cdr server)))))
+            (message (concat "re-starting github-markdown-server on port:" (number-to-string (cdr server))))
             (progn
-              (message "starting server"))
-              (setq server (cons base github-markdown-server-port))
-              ))
-          (call-process-shell-command (concat "github-markdown-server"
-                                        " --directory=" (shell-quote-argument (car server))
-                                        " --port=" (number-to-string (cdr server))
-                                        " " (shell-quote-argument file-name)) nil 0)
+              ;; find the next available port
+              (while (< 0 (length (shell-command-to-string
+                       (concat "lsof -n -i4TCP:" (number-to-string github-markdown-server-port) " | grep LISTEN"))))
+                (setq github-markdown-server-port (+ github-markdown-server-port 1)))
+
+              (message (concat "starting github-markdown-server on port:" (number-to-string (cdr server))))
+              (setq server (cons base github-markdown-server-port))))
+          (call-process-shell-command
+            (concat "github-markdown-server"
+              " --directory=" (shell-quote-argument (car server))
+              " --port=" (number-to-string (cdr server))
+              " " (shell-quote-argument file-name))
+            nil 0)
           (if (not server)
-            (progn (add-to-list 'github-markdown-servers server)
-              (setq github-markdown-server-port (+ github-markdown-server-port 1))))))))
+            (progn
+              (add-to-list 'github-markdown-servers server)
+              (setq github-markdown-server-port (+ github-markdown-server-port 1))))))
+      (message ""))))
 
 (defun view-file-in-browser (&optional file-name)
   "Open up file on github or via a local markdown server.
@@ -65,9 +76,17 @@ Prefix arg \[universal-argument] to not run local server."
   (if (and (not current-prefix-arg) (string-match "\.md$" file-name))
     (github-markdown-serve file-name)
     (call-process-shell-command
-      (concat "open "
-        (shell-quote-argument
-          (shell-command-to-string (concat "p -u -t " (shell-quote-argument file-name))))) nil 0)))
+      (concat "open -a " (shell-quote-argument github-markdown-server-browser) " \"$("
+        "perl -e 'use File::Basename; $f = shift @ARGV; $d = dirname($f); $b = basename($f); "
+        "$o = `git -C $d config --get remote.origin.url`; "
+        "if ($o =~ qr(!git(@|://)github.com[:/]!)) { "
+        "  $o =~ s!git(@|://)github.com[:/](.*).git$!https://github.com/$2!; "
+        "} else { $o = \"\" }"
+        "$p = `git -C $d rev-parse --show-prefix`; "
+        "chomp($o, $p); "
+        "if ($o) { print \"$o/blob/master/$p$b\n\"; } else { print \"file://$f\" }' "
+        (shell-quote-argument (buffer-file-name)) ")\"")
+    nil 0)))
 
 (provide 'github-markdown-server)
 ;;; github-markdown-server ends here
